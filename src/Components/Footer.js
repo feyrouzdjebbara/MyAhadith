c7dec775972e9c69dcd490c489eaa84630c06d1a
Feyrouz Djebbara <86081416+feyrouzdjebbara@users.noreply.github.com>
2023-08-26 05:35:21 -1100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer >
    
-            <div className='flex justify-between items-center flex-wrap  border-t border-gray-100  text-gray-700 sm:px-16  p-10'>
+           <div class="flex-container">
                <p>@2023 MyAhadith by Feyrouz Djb.</p>
                 <div >
                     <p> All rights reserved &copy; </p>
